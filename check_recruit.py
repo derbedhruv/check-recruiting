b219ed76a5166ea8d1b8ced35bbc4207fd3dd695
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-05-10 20:31:41 +0000

    Add request uri

--- check_recruit.py
+++ check_recruit.py
@@ -1,7 +1,8 @@
 # cache local copies of websites of companies and keep checking if something was posted by them
 # will only look at the difference between the <html> portion
 # if something is found, send an email to me
-import sys, bs4
+import sys, requests
+from bs4 import BeautifulSoup
 from smtplib import SMTP
 
 # get username and password from the login.txt file
@@ -30,13 +31,19 @@ def checksite(SITE):
   # will check given site for whether there hvae been any changes from the cached copy
   # if there has been any then will return the difference, else None
   # if there is no cached copy, then will cache the local one and do nothing
+  FILENAME = re.search('\.[a-z]+\.', SITE).group(0)[1:-1]   # returns THIS in www.THIS.whatever
   def cachecopy(s, SITE):
     # save the string s, containing the html of the page, to a local file with a special name
-    FILENAME = re.search('\.[a-z]+\.', SITE).group(0)[1:-1]   # returns THIS in www.THIS.whatever
     with open('cached/' + FILENAME + '.cachedstuff', 'w') as f:
       f.write(s)
   # read in the content of the site and only look at stuff between <body> and <body>
   # and remove any javascript and stuff
-      
+  res = requests.get(SITE)
+  s = re.search('<body>.+</body>', res.text.replace('\n', '')).group(0)
+
+  # now, if there's a cached copy, compare with it. If there isn't one, then replcae it with this
+  try:
+    with open('cached/' + FILENAME + '.cachedstuff', 'w') as f:
+      cached_copy = f.read()
 
 sendmessage('Recruiting alert', 'We found something.')
